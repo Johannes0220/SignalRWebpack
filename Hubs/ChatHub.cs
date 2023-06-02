b651b32e79d22c5310efb3fab47d51f4ce01f3e5
Johannes <johannes.schmaelzle@web.de>
2023-06-02 16:21:47 +0200

    Working App

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRWebpack.Hubs;
+
+public class ChatHub : Hub
+{
+    public async Task NewMessage(string username, string message)
+    {
+        
+        await Clients.All.SendAsync("messageReceived", username, message);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+using SignalRWebpack.Hubs;
+var builder = WebApplication.CreateBuilder(args);
+//SignalR Hinzufügen
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapHub<ChatHub>("/hub");
+
+app.Run();
