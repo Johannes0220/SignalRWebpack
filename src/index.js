f9d33774162d33ff8cfda9519f8b1abb53751821
Johannes <johannes.schmaelzle@web.de>
2023-06-14 13:49:16 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -9,14 +9,13 @@ var username = "My User";
 var connection = new signalR.HubConnectionBuilder()
     .withUrl("/hub")
     .build();
-username = connection.connectionId;
 connection.on("messageReceived", function (username, message) {
     var m = document.createElement("div");
     m.innerHTML = "<div class=\"message-author\">".concat(username, "</div><div>").concat(message, "</div>");
     divMessages.appendChild(m);
     divMessages.scrollTop = divMessages.scrollHeight;
 });
-connection.start().catch(function (err) { return document.write(err); });
+connection.start().then(function () { return username = connection.connectionId; }).catch(function (err) { return document.write(err); });
 tbMessage.addEventListener("keyup", function (e) {
     if (e.key === "Enter") {
         send();
